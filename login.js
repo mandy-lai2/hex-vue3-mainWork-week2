f25844374c7e1c8d97de0bb0ed93d0ba7fcb9568
DESKTOP-V8E7JBJ\Mandy <chjo2980@gmail.com>
2023-01-02 14:57:18 +0800

    login、products頁面更新

---
+++ login.js
@@ -0,0 +1,29 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.min.js";
+const app = {
+  data() {
+    return {
+      apiUrl:'https://vue3-course-api.hexschool.io/v2',
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      axios.post(`${this.apiUrl}/admin/signin`, this.user)
+      .then((res)=>{
+        // console.log(res.data)
+        const {token, expired}=res.data
+        document.cookie = `userToken=${ token }; expires=${ new Date(expired)};`;
+        window.location='products.html'
+      })
+      .catch((error)=>{
+        // console.log(error.data);
+        alert(error.data.message)
+      })
+    },
+  },
+  mounted() {},
+};
+createApp(app).mount("#app");
